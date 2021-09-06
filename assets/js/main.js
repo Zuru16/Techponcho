6fcb11dae6ee99db403edaeabcd97994fe1767c7
Nwabuikwu Chizuruoke <74839292+Zuru16@users.noreply.github.com>
2021-09-06 21:40:48 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function openNav() {
+  document.getElementById("overlay").style.width = "50vw";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("overlay").style.width = "0";
+}
